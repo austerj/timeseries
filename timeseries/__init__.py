f2ff7dc104f81339e9742025db56c70b22c28046
austerj <johanauster@gmail.com>
2020-09-11 19:20:54 +0200

    including doctests in unittest discovery

--- timeseries/__init__.py
+++ timeseries/__init__.py
@@ -1,5 +1,21 @@
 __docformat__ = 'restructuredtext'
 
+import warnings
+
+# submodules to include on import
 from timeseries.base import (
 	TimeSeries,
 )
+
+# submodules containing doctests
+import timeseries as ts
+doctest_modules = (
+        ts.base,
+)
+
+# include doctests in unittest discovery
+def load_tests(loader, tests, ignore):
+    import doctest
+    for submodule in doctest_modules:
+        tests.addTest(doctest.DocTestSuite(submodule))
+    return tests
